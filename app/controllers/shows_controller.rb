f8703ab8d4d745ecf026df25b4136c3cb203ec14
Graeme <flangemodo@hotmail.com>
2015-12-06 15:50:33 +0000

    add functionality to shows_controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,3 +1,45 @@
 class ShowsController < ApplicationController
+  before_action :set_show, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @shows = Show.all
+  end
+
+  def new
+    @show = Show.new
+  end
+
+  def create
+    Show.create(show_params) 
+    redirect_to(shows_path)
+  end
+
+  def show
+  end
+
+  def destroy
+    @show.destroy
+    redirect_to(shows_path)
+  end
+
+  def edit
+  end
+
+  def update
+    @show.update(show_params) 
+    redirect_to(shows_path)
+  end
+
+
+  private
+
+  def set_show
+    @show = Show.find(params[:id])
+  end
+
+  def show_params
+    params.require(:show).permit(:name, :date, :category_id)
+  end
+
 
 end
