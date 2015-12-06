72016daf1fadb9ac8f96af6eca201e8fa0fcf0dd
Graeme <flangemodo@hotmail.com>
2015-12-06 15:58:10 +0000

    add functionality to venues controller

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,45 @@
 class VenuesController < ApplicationController
+  before_action :set_venue, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @venues = Venue.all
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    Venue.create(venue_params) 
+    redirect_to(venues_path)
+  end
+
+  def show
+  end
+
+  def destroy
+    @venue.destroy
+    redirect_to(venues_path)
+  end
+
+  def edit
+  end
+
+  def update
+    @venue.update(venue_params) 
+    redirect_to(venues_path)
+  end
+
+
+  private
+
+  def set_venue
+    @venue = Venue.find(params[:id])
+  end
+
+  def show_params
+    params.require(:venue).permit(:name, :location)
+  end
+
+
 end
