e12e610a1ecc289ed60568e52d0e71dabc815c77
syed <syed.atique@gmail.com>
2015-12-06 19:20:38 +0000

    Modify Show controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,5 +1,8 @@
 class ShowsController < ApplicationController
   before_action :set_show, only: [:show, :edit, :update, :destroy]
+
+  # load_and_authorize_resource
+  # before_action :authenticate_user!
   
   def index
     @shows = Show.all
@@ -10,24 +13,44 @@ class ShowsController < ApplicationController
   end
 
   def create
-    Show.create(show_params) 
-    redirect_to(shows_path)
+    # Show.create(show_params) 
+    # redirect_to(shows_path)
+    @show = Show.new(show_params)
+
+    respond_to do |format|
+      if @show.save
+        format.html { redirect_to @show, notice: 'Show was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def show
   end
 
   def destroy
+    # @show.destroy
+    # redirect_to(shows_path)
     @show.destroy
-    redirect_to(shows_path)
+    respond_to do |format|
+      format.html { redirect_to shows_url, notice: 'Show was successfully destroyed.' }
+    end
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
   def update
-    @show.update(show_params) 
-    redirect_to(shows_path)
+    # @show.update(show_params) 
+    # redirect_to(shows_path)
+    respond_to do |format|
+      if @show.update(show_params)
+        format.html { redirect_to @show, notice: 'Show was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
 
