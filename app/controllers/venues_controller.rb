4056d57ddbf7dfab1218b29baa0984a0f682aec2
syed <syed.atique@gmail.com>
2015-12-06 19:25:18 +0000

    Modify Venue controller

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,5 +1,8 @@
 class VenuesController < ApplicationController
   before_action :set_venue, only: [:show, :edit, :update, :destroy]
+
+  # load_and_authorize_resource
+  # before_action :authenticate_user!
   
   def index
     @venues = Venue.all
@@ -10,24 +13,44 @@ class VenuesController < ApplicationController
   end
 
   def create
-    Venue.create(venue_params) 
-    redirect_to(venues_path)
+    # Venue.create(venue_params) 
+    # redirect_to(venues_path)
+    @venue = Venue.new(venue_params)
+
+    respond_to do |format|
+      if @venue.save
+        format.html { redirect_to @venue, notice: 'Venue was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def show
   end
 
   def destroy
+    # @venue.destroy
+    # redirect_to(venues_path)
     @venue.destroy
-    redirect_to(venues_path)
+    respond_to do |format|
+      format.html { redirect_to venues_url, notice: 'Venue was successfully destroyed.' }
+    end
   end
 
   def edit
   end
 
   def update
-    @venue.update(venue_params) 
-    redirect_to(venues_path)
+    # @venue.update(venue_params) 
+    # redirect_to(venues_path)
+    respond_to do |format|
+      if @venue.update(venue_params)
+        format.html { redirect_to @venue, notice: 'Venue was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
 
