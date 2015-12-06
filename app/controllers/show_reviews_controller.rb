7a5f5b9031ed7e0df6ddddc0e82279525a82d385
Graeme <flangemodo@hotmail.com>
2015-12-06 14:56:50 +0000

    added show review view

--- app/controllers/show_reviews_controller.rb
+++ app/controllers/show_reviews_controller.rb
@@ -1,2 +1,44 @@
 class ShowReviewsController < ApplicationController
+
+  def index
+    @show_reviews = Show_reviews.all
+  end
+
+  def new
+    @show_reviews = Show_reviews.new
+  end
+
+  def create
+    Show_reviews.create(show_review_params) 
+    redirect_to(show_reviews_path)
+  end
+
+  def show
+  end
+
+  def destroy
+    @show_review.destroy
+    redirect_to(show_reviews_path)
+  end
+
+  def edit
+  end
+
+  def update
+    @show_review.update(show_review_params) 
+    redirect_to(show_reviews_path)
+  end
+
+
+  private
+
+  def set_show_review
+    @show_review = Show_review.find(params[:id])
+  end
+
+  def show_review_params
+    params.require(:show_review).permit(:title, :show_id, :author, :description, :rating, :user_id)
+  end
+
+
 end
