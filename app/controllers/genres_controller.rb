3384cbd9fcee5427fcdaff2b319e036a7f333149
Graeme <flangemodo@hotmail.com>
2015-12-07 10:56:17 +0000

    added functionality to controller

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,4 +3,43 @@ class GenresController < ApplicationController
   def index
     @genres = Genre.all
   end
+
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    Genre.create(genre_params) 
+    redirect_to(genres_path)
+  end
+
+  def show
+  end
+
+  def destroy
+    @genre.destroy
+    redirect_to(genres_path)
+  end
+
+  def edit
+  end
+
+  def update
+    @genre.update(genre_params) 
+    redirect_to(genres_path)
+  end
+
+
+  private
+
+  def set_genre
+    @genre = Genre.find(params[:id])
+  end
+
+  def genre_params
+    params.require(:genre).permit(:name, :live_music)
+  end
+
+
 end
--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,2 +1,4 @@
 class PerformancesController < ApplicationController
+
+  
 end
