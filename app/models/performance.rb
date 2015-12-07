734761406b6ce53161baf71fd59bdcaee0b7ddc2
syed <syed.atique@gmail.com>
2015-12-07 17:34:43 +0000

    binding pry removed from performance.rb

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -10,7 +10,7 @@ class Performance < ActiveRecord::Base
   def search_result(name)
     show_matches = Show.where("name like ?", "%#{name}%")
     venue_matches = Venue.where("name like ?", "%#{name}%")
-      binding.pry
+      # binding.pry
     performance_show = show_matches.map {|show| Performance.where(:show_id => show.id) }
     performance_venue = venue_matches.map {|venue| Performance.where(:venue_id => venue.id) }
     ((performance_show + performance_venue).flatten).uniq
