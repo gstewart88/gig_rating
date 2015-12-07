7b130f9f7b633b20d124afc9e7aab85c45f6a990
syed <syed.atique@gmail.com>
2015-12-07 12:30:24 +0000

    changes to applicstion controller to raise exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   #If the user authorization fails, a CanCan::AccessDenied exception will be raised.
+  
   rescue_from CanCan::AccessDenied do |exception|
-    respond_to do |format|
-      format.json { render nothing: true, status: :forbidden }
-      format.html { redirect_to main_app.root_url, :alert => exception.message }
-    end
+   redirect_to root_path, alert: "You can't access this page"
   end
+
 end
