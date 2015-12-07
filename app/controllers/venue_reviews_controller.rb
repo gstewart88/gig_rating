04d5644e5a538f2cb1699fc396d2ad0f3da491c6
Graeme <flangemodo@hotmail.com>
2015-12-07 10:46:13 +0000

    filled out the venue review controller

--- app/controllers/venue_reviews_controller.rb
+++ app/controllers/venue_reviews_controller.rb
@@ -1,2 +1,44 @@
 class VenueReviewsController < ApplicationController
+
+  def index
+    @venue_reviews = VenueReview.all
+  end
+
+  def new
+    @venue_reviews = VenueReview.new
+  end
+
+  def create
+    VenueReview.create(venue_review_params) 
+    redirect_to(venue_reviews_path)
+  end
+
+  def show
+  end
+
+  def destroy
+    @venue_review.destroy
+    redirect_to(venue_reviews_path)
+  end
+
+  def edit
+  end
+
+  def update
+    @venue_review.update(venue_review_params) 
+    redirect_to(venue_reviews_path)
+  end
+
+
+  private
+
+  def set_venue_review
+    @venue_review = VenueReview.find(params[:id])
+  end
+
+  def venue_review_params
+    params.require(:venue_review).permit(:title, :venue_id, :author, :description, :rating, :user_id)
+  end
+
+
 end
