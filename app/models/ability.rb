a37ab6f0ed15248867760a3981a9feeb9c1bf405
syed <syed.atique@gmail.com>
2015-12-08 13:23:59 +0000

    Added Cancan User permission in Ability.rb file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,13 @@ class Ability
   
   def initialize(user)
     user ||= User.new
+    alias_action :read, :create, :to =>cread
     if user.role? :admin
       can :manage, :all
     elsif
       user.role?  :registered
-      can :manage, :all
+      can :cread, :all
+      can :update, Review
     else
       can :read, :all
 
